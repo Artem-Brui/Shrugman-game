8b18e9a28b20060ed21ce9aefe611e6f935a7454
Artem-Brui <artembryj@gmail.com>
2024-07-16 14:53:29 +0200

    created function startNewRound for filter users and save new ones

--- main.js
+++ main.js
@@ -5,8 +5,11 @@
 const LINK_SPEAKING = require('./game-data/speaking.json');
 const LINK_TITLES = require('./game-data/titles.json');
 const LINK_PICTURES = require('./game-data/pictures.json');
-
 var readlineSync = require('readline-sync');
+
+//#endregion ==============
+
+//#region VARIABLES 
 
 const movies = [...LINK_TITLES.movies];
 const books = [...LINK_TITLES.books];
@@ -14,10 +17,30 @@ const gamers = [];
 
 //#endregion ==============
 
+//#region CLASSES 
+
+class newGamer {
+  constructor (name, movies,books, lastGameDate) {
+      this.name = name;
+      this.movies = movies;
+      this.books = books;
+      this.lastGameDate = lastGameDate;
+  }
+}
+
+class titlesList {
+  constructor (finishedGames, futureGames) {
+      this.finishedGames = finishedGames;
+      this.futureGames = futureGames;
+  }
+}
+
+//#endregion ==============
+
 //#region FUNCTION greetingGame() that say hello and get a name of user
 function greetingGame() {
   console.clear();
-  return `${LINK_PICTURES.welcome}\n\n${LINK_SPEAKING.greeting}\n`;
+  return `${LINK_PICTURES.smile}\n\n${LINK_PICTURES.welcome}\n\n${LINK_SPEAKING.greeting}\n`;
 }
 //#endregion ==============
 
@@ -41,11 +64,32 @@ function nameChanger() {
 }
 //#endregion ==============
 
-//#region FUNCTION isGamerExist() that check is user already exist in the game
-function isGamerExist(name) {
+//#region FUNCTION startNewRound() that check is user already exist in the game
+function startNewRound(newUserName, repeatMarker) {
   console.clear();
-  console.log(`${LINK_PICTURES.welcome}\n`);
+  
+  let userInput = '';
+  const filterFunction = gamer => newUserName === gamer['name'];
 
+  if (repeatMarker > 0) {
+    gamers.filter(filterFunction).length === 0
+    ? (console.log(`${LINK_PICTURES.thinking}\n`),
+      userInput = readlineSync.question(LINK_SPEAKING.gameForNewUserRepeat))
+    : (console.log(`${LINK_PICTURES.thinking}\n`),
+      userInput = readlineSync.question(LINK_SPEAKING.gameForOldUserRepeat));
+
+    return userInput;
+  }
+
+
+  gamers.filter(filterFunction).length === 0
+    ? (console.log(`${LINK_PICTURES.smile}\n`),
+      userInput = readlineSync.question(LINK_SPEAKING.gameForNewUser))
+    : (console.log(`${LINK_PICTURES.smile}\n`),
+      userInput = readlineSync.question(LINK_SPEAKING.gameForOldUser));
+
+  return userInput;
+  //could be "start" or "go"
 }
 //#endregion ==============
 
@@ -55,26 +99,40 @@ const gameStart = () => gameSteps();
 
 //#endregion ==============
 
-
-
 //#region FUNCTION gameSteps() go throw all steps of a game
 function gameSteps(name) {
   console.log(greetingGame());
 
+  // user name input
   let currentUserName = readlineSync.question(LINK_SPEAKING.nameInputStart);
 
+  //name validation
   while (!nameIsValid(currentUserName)) {
     currentUserName = nameChanger();
   };
 
-  console.clear();
-  console.log(LINK_SPEAKING.userNotFound);
+  // start new round user input
+  let currentUserGameStartInput = startNewRound(currentUserName);
+  switch (currentUserGameStartInput) {
+    case "start":
+      console.log('1');
+      break;
+    case "go":
+      console.log('2');
+      break;
+    default:
+      startNewRound(currentUserName, 1)
+      break;
+  }
+
+  
+  
 }
 //#endregion ==============
 
+gameStart()
 
-  
-gameStart();
+// startNewRound('tom1')
 
 
 
