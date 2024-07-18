d699cbba26ad9f3c6343cc566f068c80a8f9af18
Artem-Brui <artembryj@gmail.com>
2024-07-18 11:54:15 +0200

    game logik was created

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const LINK_GAMERS_LIST = require('./content/gamers.json');
 
 
 var readlineSync = require('readline-sync');
-const FS = require('fs');
+var FS = require('fs');
 const Gamer = require('./scripts/class-gamer.js');
 
 
@@ -29,7 +29,31 @@ const pathGamersFile = './content/gamers.json';
 
 //#region FUNCTIONS 
 
+//#region ====== THE GAME STEPS
 
+//#region FUNCTION roadMap() go through all steps of a game
+
+function roadMap() {
+  console.log(greetingGame());
+
+  // user name input
+  let currentUserName = userAnswer(LINK_SPEAKING.nameInputStart);
+
+  //name validation
+   while (!nameIsValid(currentUserName)) {
+    currentUserName = nameChanger();
+  };
+
+  // start new round
+  const commandToStart = getCommandToStart(currentUserName);
+  startNewRound(currentUserName, commandToStart, pathGamersFile);
+
+}
+//#endregion ==============
+
+
+
+//#endregion ==============
 
 
 //#region ===== PREPARING BEFORE THE GAME
@@ -85,11 +109,11 @@ function nameChanger() {
 
 //#region FUNCTION isUserNameExist() that check is user already exist in the game and return true or false
 
-const isUserNameExist = (pathToGamers, userName) => pathToGamers.gamers.some(gamer => gamer["name"] === userName);
+const isUserNameExist = userName => pathGamersFile.gamers.some(gamer => gamer["name"] === userName);
 
 //#endregion ==============
 
-//#region FUNCTION takeCommandToStart() that get and return user input to start new round
+//#region FUNCTION getCommandToStart() that get and return user input to start new round
 
 function getCommandToStart(newUserName, repeatMarker) {
   console.clear();
@@ -112,7 +136,7 @@ function getCommandToStart(newUserName, repeatMarker) {
     userInput = userAnswer(LINK_SPEAKING.gameForOldUser));
 
   return userInput;
-    //can be "start" or "go"
+    //can be "st" or "go"
 }
 
 //#endregion ==============
@@ -143,6 +167,8 @@ function changeGamersList(pathToGamers, newUserName, action) {
           }
         }
         break;
+      case 'add-new-game-to-user':
+        pathToGamers.addNewGame(title, categoryName, gameStatus, lastGameDate)
       default:
         break;
     }
@@ -156,6 +182,13 @@ function changeGamersList(pathToGamers, newUserName, action) {
   });
 }
 
+// const obj = {...LINK_GAMERS_LIST.gamers[1]};
+// obj.addNewGame('title', 'categoryName', 'gameStatus', 'lastGameDate')
+
+// console.clear();
+// console.log(obj);
+// changeGamersList(LINK_GAMERS_LIST, 'ar', 'add-new-game-to-user')
+
 //#endregion ==============
 
 //#region FUNCTION addGameToUser() 
@@ -166,13 +199,13 @@ function changeGamersList(pathToGamers, newUserName, action) {
 
 //#endregion ==============
 
-//#region FUNCTION startNewRound() 
+//#region FUNCTION startNewRound() !!!
 
 function startNewRound(userName, startInput, pathToGamers) {
   switch (startInput) {
     case "st":
       changeGamersList(pathToGamers, userName, 'add-new-gamer');
-      //changeGamersList(pathToGamers, userName, 'delete-gamer');
+      //getGameTitle(userName, 'movies');
       break;
     case "go":
       console.log('2');
@@ -185,12 +218,6 @@ function startNewRound(userName, startInput, pathToGamers) {
 
 //#endregion ==============
 
-//#region FUNCTION startNewRound() 
-
-
-
-//#endregion ==============
-
 // {
 //   "gamers": [
 //     { "id": 1, "name": "InitialUser-BOT", "games": [] }]
@@ -199,49 +226,106 @@ function startNewRound(userName, startInput, pathToGamers) {
 //#endregion ==============
 
 
-
-
 //#region ====== THE GAME
 
+//#region FUNCTION getGameTitle() that return a title(string) for new game and put it to user list as object with full data about game. Gamer must exist in the file gamers
 
+function getGameTitle(userName, category) {
+  console.clear();
+  let titleForGame;
+  const userGameList = LINK_GAMERS_LIST.gamers.filter((user) => user.name === userName)[0].games;
+  const fullGameList = LINK_TITLES[category];
+
+  do {
+    const randomIndex = Math.floor(Math.random() * fullGameList.length)
+    titleForGame = fullGameList[randomIndex];
+  } while (userGameList.includes(titleForGame))
+
+  return titleForGame
+}
+
+//#endregion
+
+//#region FUNCTION game(title)
+
+function game(title) {
+  console.clear()
+  let isGameWinner = false;
+  const shragman = '¯\\_(:/)_/¯';
+
+  const titleHider = title => {
+    const pattern = /^[a-zA-Z]/
+    let hidenLetter = '';
+    for (character of title) {
+      pattern.test(character) 
+        ? hidenLetter += '_'
+        : hidenLetter += character;
+      }
+    return hidenLetter
+  }
+
+  const titleRevealer = (title, hidenTittle, letter) => {
+    let updatedTitle = '';
+    let letterIsFound = false;
+
+    for (let i = 0; i < title.length; i++) {
+      title[i].toLowerCase() === letter.toLowerCase()
+        ? (updatedTitle += title[i], letterIsFound = true)
+        : updatedTitle += hidenTittle[i];
+    }
+
+    if (!letterIsFound && gameShragman.length !== shragman.length) {
+      gameShragman += shragman[gameShragman.length];
+    }
+
+    return updatedTitle;
+  }
+
+  let gameShragman = ''
+  let gameTitle = titleHider(title);
+  
+  do {
+    console.clear()
+    console.log(`\n${gameTitle}\n`);
+    console.log(`\n${gameShragman}\n`);
+    let letterForChecking = '';
+    let needToRepeat = false;
+    do {
+      console.clear()
+      console.log(`\n${gameTitle}\n`);
+      console.log(`\n${gameShragman}\n`);
+
+      needToRepeat
+        ? letterForChecking = userAnswer(`${LINK_SPEAKING.repeatChoiceWithLetter}`)
+        : letterForChecking = userAnswer(`${LINK_SPEAKING.makeChoiceWithLetter}`);
+      
+      needToRepeat = true;
+    } while (letterForChecking.length !== 1)
+    //
+
+    gameTitle = titleRevealer(title, gameTitle, letterForChecking);
+  } while (gameTitle.includes('_'));
+
+  console.clear()
+  console.log(`\n${gameTitle}\n`);
+  console.log(`${LINK_SPEAKING.winner}`);
+  isGameWinner = true;
+
+    return titleHider(title)
+}
+
+//#endregion
 
 
 
 //#endregion
 
 
-
-
-//#region ====== THE GAME STEPS
-
-//#region FUNCTION roadMap() go through all steps of a game
-
-function roadMap() {
-  console.log(greetingGame());
-
-  // user name input
-  let currentUserName = userAnswer(LINK_SPEAKING.nameInputStart);
-
-  //name validation
-  while (!nameIsValid(currentUserName)) {
-    currentUserName = nameChanger();
-  };
-  // start new round user input
-  const commandToStart = getCommandToStart(currentUserName);
-  startNewRound(currentUserName, commandToStart, pathGamersFile);
-
-
-}
-//#endregion ==============
-
-
-
-//#endregion ==============
-
 //#endregion ======FUNCTIONS======
 
-
-gameStart()
+console.log(getGameTitle('ar', 'movies'));
+console.log(game('Forrest Gump 2'))
+//gameStart()
 
 
 
